6283354cf93e7a9ed6afc1b0310b6b65d820a74f
vincenzoperisco05 <133646418+vincenzoperisco05@users.noreply.github.com>
2023-05-15 11:24:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function searchNames() {
+    var input = document.getElementById('search-input').value.toLowerCase();
+    var names = ['Nicolò Carcagnì', 'Vincenzo Persico', 'Anna Spedicato']; //Nomi
+    var results = document.getElementById('search-results');
+    results.innerHTML = '';
+
+    for (var i = 0; i < names.length; i++) {
+      var name = names[i].toLowerCase();
+      if (name.includes(input)) {
+        var listItem = document.createElement('li');
+        listItem.textContent = names[i];
+        listItem.className = 'suggestion';
+        listItem.addEventListener('click', function() {
+          document.getElementById('search-input').value = this.textContent;
+          results.innerHTML = '';
+        });
+        results.appendChild(listItem);
+      }
+    }
+  }
