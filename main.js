31b3bac8c0523f3b252b01793e38162d2dfc5fd4
vincenzopersico05 <133646418+vincenzopersico05@users.noreply.github.com>
2023-05-16 20:59:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 function searchNames() {
   var input = document.getElementById('search-input').value.toLowerCase();
-  var names = ['Nicolò Carcagnì', 'Vincenzo Persico', 'Anna Spedicato']; //Nomi
+  var names = ['Nicolò Carcagnì', 'Vincenzo Persico', 'Anna Spedicato']; // Nomi
+  var tableNumbers = ['~ Gloria Gaynor ~', '~ Gloria Gaynor ~', '~ Gloria Gaynor ~']; // Numeri di tavolo corrispondenti
   var results = document.getElementById('search-results');
   results.innerHTML = '';
 
   for (var i = 0; i < names.length; i++) {
     var name = names[i].toLowerCase();
-    if (name.includes(input)) {
+    if (input==''){
+      document.getElementById('tavolo').innerHTML ="";
+      document.getElementById("iltuo").style.visibility = 'hidden';
+    }
+    else if (name.includes(input)) {
       var listItem = document.createElement('li');
       listItem.textContent = names[i];
       listItem.className = 'suggestion';
       listItem.addEventListener('click', function() {
         document.getElementById('search-input').value = this.textContent;
-        results.innerHTML = '';
+        let invitato = this.textContent;
+        var nameAndTable = '';
+        for (var i = 0; i < names.length; i++) {
+        if (invitato==names[i]){
+          nameAndTable = tableNumbers[i];
+        }
+        }
+        document.getElementById('tavolo').innerHTML = nameAndTable;
+        document.getElementById("iltuo").style.visibility = 'visible';
       });
       results.appendChild(listItem);
-    }
-  }
+    }
+  }
 }
